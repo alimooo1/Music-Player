3e753acdb01c7cce6be12de9615d31fc068940c5
Ali Moallemi <alimoallemi30@gmail.com>
2023-05-16 08:20:04 +0330

    some bugs in index.ts fixed with bind method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import "./style.scss";
+import "./style.scss";
 
 const songName = document.querySelector(".song-name") as HTMLDivElement;
 const artistName = document.querySelector(".artist-name") as HTMLDivElement;
@@ -80,7 +80,10 @@ class MusicPlayer {
     playPause.classList.remove("fa-circle-play");
     playPause.classList.add("fa-circle-pause");
     audio.play();
-    this._kornometer = window.setInterval(this._currentTimeHandler, 1000);
+    this._kornometer = window.setInterval(
+      this._currentTimeHandler.bind(this),
+      1000
+    );
   }
 
   private static _pauseSong() {
@@ -95,9 +98,9 @@ class MusicPlayer {
     songName.innerText = songs[this._songIndex].name;
     artistName.innerText = songs[this._songIndex].artist;
     audio.setAttribute("src", songs[this._songIndex].src);
-    const convertedTime = this._convertTime(audio.duration);
+    const convertedTime = this._convertTime.bind(this);
     audio.addEventListener("loadeddata", function () {
-      time.innerText = convertedTime;
+      time.innerText = convertedTime(audio.duration);
     });
     currentTime.innerText = "00:00";
     body.style.backgroundImage = `url(${songs[this._songIndex].background})`;
@@ -156,56 +159,57 @@ class MusicPlayer {
     currentTime.innerHTML = this._convertTime(audio.currentTime);
   }
 
-  private static _hoverHandler() {
-    playerBox.style.backgroundColor = "rgb(45, 118, 187)";
-    playerBox.style.cursor = "pointer";
-    playerBox.style.boxShadow =
-      "1px 1px 2px black, 0 0 5px rgb(13, 44, 5), 0 0 5px rgba(1, 139, 30, 0.614)";
-  }
-
-  private static _unHoverHandler() {
-    playerBox.style.backgroundColor = "rgba(121, 240, 168,0)";
-    playerBox.style.border = "4px solid rgb(45, 118, 187)";
-    playerBox.style.cursor = "default";
-    playerBox.style.boxShadow = "";
-  }
-
-  private static _firstShowHandler() {
-    playerBox.style.backgroundColor = "rgba(237, 255, 242,0)";
-    playerBox.style.border = "2px solid rgb(45, 118, 187)";
-    playerBox.style.boxShadow = "";
-    playerBox.style.animation = "first-show 3s";
-    playerBox.removeEventListener("mouseover", this._hoverHandler);
-    playerBox.removeEventListener("mouseout", this._unHoverHandler);
-    playerBox.removeEventListener("click", this._firstShowHandler);
-    playerBox.style.cursor = "default";
-    playerBox.style.width = "456px";
-    playerBox.style.height = "80%";
-    playerBox.style.transition = "all 1s";
-    content.style.animation = "fade-in 2s";
-    playerBox.style.boxShadow = "";
-  }
-
-  private static _animationHandler() {
-    playerBox.style.border = "none";
-    playerBox.style.boxShadow = "";
-    body.style.backgroundImage = "url('../videos/believergif.gif')";
-    playerBox.style.backgroundColor = "rgba(90, 133, 105, 0.178)";
-    content.style.display = "block";
-    content.style.visibility = "visible";
-    content.style.opacity = "1";
-  }
-
   public static Run() {
-    playPause.addEventListener("click", this._playPauseHandler);
-    forward.addEventListener("click", this._nextMusic);
-    backward.addEventListener("click", this._previousSong);
-    timeline.addEventListener("click", this._selectTime);
-    playerBox.addEventListener("click", this._firstShowHandler);
-    playerBox.addEventListener("mouseover", this._hoverHandler);
-    playerBox.addEventListener("mouseout", this._unHoverHandler);
-    playerBox.addEventListener("animationend", this._animationHandler);
+    document.body.style.backgroundImage = "url('../videos/visulizegif.gif')";
+    playPause.addEventListener("click", this._playPauseHandler.bind(this));
+    forward.addEventListener("click", this._nextMusic.bind(this));
+    backward.addEventListener("click", this._previousSong.bind(this));
+    timeline.addEventListener("click", this._selectTime.bind(this));
+    playerBox.addEventListener("click", firstShowHandler);
+    playerBox.addEventListener("mouseover", hoverHandler);
+    playerBox.addEventListener("mouseout", unHoverHandler);
+    playerBox.addEventListener("animationend", animationHandler);
   }
+}
+
+function hoverHandler() {
+  playerBox.style.backgroundColor = "rgb(45, 118, 187)";
+  playerBox.style.cursor = "pointer";
+  playerBox.style.boxShadow =
+    "1px 1px 2px black, 0 0 5px rgb(13, 44, 5), 0 0 5px rgba(1, 139, 30, 0.614)";
+}
+
+function unHoverHandler() {
+  playerBox.style.backgroundColor = "rgba(121, 240, 168,0)";
+  playerBox.style.border = "4px solid rgb(45, 118, 187)";
+  playerBox.style.cursor = "default";
+  playerBox.style.boxShadow = "";
+}
+
+function firstShowHandler() {
+  playerBox.style.backgroundColor = "rgba(237, 255, 242,0)";
+  playerBox.style.border = "2px solid rgb(45, 118, 187)";
+  playerBox.style.boxShadow = "";
+  playerBox.style.animation = "first-show 3s";
+  playerBox.removeEventListener("mouseover", hoverHandler);
+  playerBox.removeEventListener("mouseout", unHoverHandler);
+  playerBox.removeEventListener("click", firstShowHandler);
+  playerBox.style.cursor = "default";
+  playerBox.style.width = "456px";
+  playerBox.style.height = "80%";
+  playerBox.style.transition = "all 1s";
+  content.style.animation = "fade-in 2s";
+  playerBox.style.boxShadow = "";
+}
+
+function animationHandler() {
+  playerBox.style.border = "none";
+  playerBox.style.boxShadow = "";
+  body.style.backgroundImage = "url('../videos/believergif.gif')";
+  playerBox.style.backgroundColor = "rgba(90, 133, 105, 0.178)";
+  content.style.display = "block";
+  content.style.visibility = "visible";
+  content.style.opacity = "1";
 }
 
 MusicPlayer.Run();
